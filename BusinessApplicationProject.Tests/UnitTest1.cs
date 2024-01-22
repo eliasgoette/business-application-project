5bae36e439c62cc44bcbb6d2f722d9295a9d1f2c
Elias G <83478605+eliasgoette@users.noreply.github.com>
2024-01-22 16:33:29 +0100

    Added empty test suite to solution

---
+++ BusinessApplicationProject.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BusinessApplicationProject.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
