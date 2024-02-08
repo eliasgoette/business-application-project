1ff9f0930a7e3077994e233b47dc65f1949dfd51
Elias G <83478605+eliasgoette@users.noreply.github.com>
2024-02-08 18:32:00 +0100

    Started implementing recursive CTE for AGs

---
+++ BusinessApplicationProject/Repository/ArticleGroupRepository.cs
@@ -0,0 +1,29 @@
+﻿using BusinessApplicationProject.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessApplicationProject.Repository
+{
+    public class ArticleGroupRepository : Repository<ArticleGroup>
+    {
+        public ArticleGroupRepository(AppDbContext context) : base(context) { }
+
+        public List<ArticleGroup> GetHierarchy()
+        {
+            string cteQuery = @"
+                ;WITH ArticleGroupHierarchy AS (
+                    SELECT Id, Name, ParentId
+                    FROM ArticleGroups
+                    WHERE ParentId IS NULL
+                    UNION ALL
+                    SELECT ag.Id, ag.Name, ag.ParentId
+                    FROM ArticleGroups ag
+                    INNER JOIN ArticleGroupHierarchy agh ON ag.ParentId = agh.Id
+                )
+                SELECT * FROM ArticleGroupHierarchy";
+
+            List<ArticleGroup> cteRes = Context.ArticleGroups.FromSqlRaw(cteQuery).ToList();
+
+            return cteRes;
+        }
+    }
+}
