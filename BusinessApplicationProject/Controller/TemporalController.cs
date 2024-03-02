ca0a735b9647397149383bcbf13c06fc68c54376
Elias G <83478605+eliasgoette@users.noreply.github.com>
2024-03-03 00:15:15 +0100

    Added TemporalController

---
+++ BusinessApplicationProject/Controller/TemporalController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessApplicationProject.Repository;
+using System.Linq.Expressions;
+
+namespace BusinessApplicationProject.Controller
+{
+    class TemporalController<T> : Controller<T> where T : class
+    {
+        // Make the repository temporal (also has inherited capabilities from normal repository)
+        public delegate TemporalRepository<T> RepositoryFactory(AppDbContext context);
+        public RepositoryFactory getRepository { get; init; }
+
+        public List<T> FindAsOf(DateTime timestamp, Expression<Func<T, bool>> condition)
+        {
+            List<T> result = new List<T>();
+
+            using (var context = getContext())
+            {
+                var repository = getRepository(context);
+                result = repository.FindAsOf(timestamp, condition).ToList();
+            }
+
+            return result;
+        }
+    }
+}
--- BusinessApplicationProject/Repository/ITemporalRepository.cs
+++ BusinessApplicationProject/Repository/ITemporalRepository.cs
@@ -1,9 +1,11 @@
-﻿namespace BusinessApplicationProject.Repository
+﻿using System.Linq.Expressions;
+
+namespace BusinessApplicationProject.Repository
 {
     public interface ITemporalRepository<T> : IRepository<T> where T : class
     {
         public IEnumerable<T> GetAllAsOf(DateTime timestamp);
 
-        public IEnumerable<T> FindAsOf(DateTime timestamp, Func<T, bool> condition);
+        public IEnumerable<T> FindAsOf(DateTime timestamp, Expression<Func<T, bool>> condition);
     }
 }
